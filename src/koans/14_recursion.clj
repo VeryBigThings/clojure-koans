670c21a213b959da72328f55b8e7542d5e0412d5
knikoletic <karlo.nikoletic@verybigthings.com>
2021-12-18 20:43:26 +0100

    Solve exercise 14

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -3,21 +3,27 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      false
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [[head & tail] coll new []]
+    (if-not head
+      new
+      (recur tail (cons head new)))))
 
 (defn factorial [n]
-  __)
+  (loop [no n acc 1]
+    (if (zero? no)
+      acc
+      (recur (dec no) (* acc no)))))
 
 (meditations
   "Recursion ends with a base case"
