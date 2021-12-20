87feb13591ade3890a891dea8de5d90eccd620b7
Kenneth Kostresevic <kenneth.kostresevic@verybigthings.com>
2021-12-20 13:09:29 +0100

    Use apply and interpose

--- src/koans/15_destructuring.clj
+++ src/koans/15_destructuring.clj
@@ -22,7 +22,7 @@
   (= "Rich Hickey aka The Clojurer aka Go Time aka Lambda Guru"
      (let [[first-name last-name & aliases]
            (list "Rich" "Hickey" "The Clojurer" "Go Time" "Lambda Guru")]
-        (str first-name " " last-name (reduce str (map #(str " aka " %) aliases)))))
+        (str first-name " " last-name " aka " (apply str (interpose " aka " aliases)))))
 
   "You can regain the full argument if you like arguing"
   (= {:original-parts ["Stephen" "Hawking"] :named-parts {:first "Stephen" :last "Hawking"}}
